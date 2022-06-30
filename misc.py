cb0922abec569cecc75c699bcaa1abdecec1805f
pytera895143242 <91798465+pytera895143242@users.noreply.github.com>
2022-06-30 17:39:51 +0500

    gr

---
+++ misc.py
@@ -0,0 +1,12 @@
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+
+TOKEN = '5538054227:AAF7N1pa5aYy3x3GVte66iCbZGY4mkzJ7PI'
+memory_storage = MemoryStorage()
+
+bot = Bot(token=TOKEN,parse_mode='html')
+dp = Dispatcher(bot,storage=memory_storage)
+logging.basicConfig(level=logging.INFO)
